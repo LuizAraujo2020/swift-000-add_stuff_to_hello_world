6e77df539dc58cdca901dd4bbfd007129e44c6da
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-11 22:42:50 -0300

    Initial Commit

---
+++ swift-000-add_stuff_to_hello_worldTests/swift_000_add_stuff_to_hello_worldTests.swift
@@ -0,0 +1,33 @@
+//
+//  swift_000_add_stuff_to_hello_worldTests.swift
+//  swift-000-add_stuff_to_hello_worldTests
+//
+//  Created by Luiz Carlos da Silva Araujo on 11/08/21.
+//
+
+import XCTest
+@testable import swift_000_add_stuff_to_hello_world
+
+class swift_000_add_stuff_to_hello_worldTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

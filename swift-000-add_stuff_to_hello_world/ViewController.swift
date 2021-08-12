b52ee0236b40a53c526140b2271ad9a93a9f9aa3
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-12 00:13:30 -0300

    :gift: dec2bin

--- swift-000-add_stuff_to_hello_world/ViewController.swift
+++ swift-000-add_stuff_to_hello_world/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    private var decimal: Int = 0
+//    private var binario: String = "00000"
+    
+    @IBOutlet weak var label_result: UILabel!
+    @IBOutlet weak var textfield_decimal: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func textfield_decimal(_ sender: Any) {
+        
+    }
+    
+    @IBAction func hello(_ sender: Any) {
+        let decimal = textfield_decimal.text ?? "0"
+        print("DECIMAL: \(decimal)")
+        let binario = String(Int(decimal) ?? 0, radix: 2)
+        print("BINARIO: \(binario)") // decimal 22 prints "10110"
+        
+        label_result.text = binario
+        
+    }
+    
+    
+    
+    
 }
 
